c993ae3bafd2845d6ed951a09fd14a0b84927266
charcole <charlie@vyse.me.uk>
2016-08-14 08:51:55 +0100

    Single cycle accurate delay

--- Zapper.ino
+++ Zapper.ino
@@ -23,76 +23,47 @@ void setup() {
   UCSR0B&=~((1<<7)|(1<<5)); // Clear RX complete + UDR empty interrupt
 }
 
-short ProcessLine(short delayValue)
+void ProcessLine(short delayValue)
 {
-  unsigned char low=delayValue&255;
-  unsigned char high=delayValue>>8;
+  unsigned char lowBits=delayValue&3;
+  unsigned char fours=(delayValue>>2)-1;
   unsigned char counter=4; // Make sure sync has settled at high level
-  if (high==0)
-  {
-    asm volatile
-    (
-      "      CLI\n"
-      "LOOPA:\n"
-      "      SBIS %4, %5\n"  // Wait for sync high
-      "      RJMP LOOPA\n"
-      "      DEC %1\n"
-      "      BRNE LOOPA\n"
-      "LOOPB:\n"
-      "      SBIC %4, %5\n"  // Wait for sync low
-      "      RJMP LOOPB\n"
-      "LOOPC:\n"
-      "      DEC %0\n"   // 1 cycles
-      "      BRNE LOOPC\n"    // 2 cycles when true
-      "      SBIC %4, %6\n"
-      "      SBI %2, %3\n"
-      "      CBI %2, %7\n"
-      "      NOP\n"
-      "      NOP\n"
-      "      NOP\n"
-      "      NOP\n"
-      "      SBI %2, %7\n"
-      "      SEI\n"
-      : "+r" (low), "+r" (counter)
-      : "I" (_SFR_IO_ADDR(PORTD)), "I" (PORTD4), "I" (_SFR_IO_ADDR(PIND)), "I" (PIND6), "I" (PIND5), "I" (PORTD3)
-    );
-  }
-  else if (high==1)
-  {
-    unsigned char twofivefive=255;
-    asm volatile
-    (
-      "      CLI\n"
-      "LOOPX:\n"
-      "      SBIS %5, %6\n"  // Wait for sync high
-      "      RJMP LOOPX\n"
-      "      DEC %2\n"
-      "      BRNE LOOPX\n"
-      "LOOPY:\n"
-      "      SBIC %5, %6\n"  // Wait for sync low
-      "      RJMP LOOPY\n"
-      "LOOPZ:\n"
-      "      DEC %1\n"   // 1 cycles
-      "      BRNE LOOPZ\n"    // 2 cycles when true
-      "LOOPW:\n"
-      "      DEC %0\n"   // 1 cycles
-      "      BRNE LOOPW\n"    // 2 cycles when true
-      "      SBIC %5, %7\n"
-      "      SBI %3, %4\n"
-      "      CBI %3, %8\n"
-      "      NOP\n"
-      "      NOP\n"
-      "      NOP\n"
-      "      NOP\n"
-      "      SBI %3, %8\n"
-      "      SEI\n"
-      : "+r" (low), "+r" (twofivefive), "+r" (counter)
-      : "I" (_SFR_IO_ADDR(PORTD)), "I" (PORTD4), "I" (_SFR_IO_ADDR(PIND)), "I" (PIND6), "I" (PIND5), "I" (PORTD3)
-    );
-  }
+  asm volatile
+  (
+    "      CLI\n"
+    "LOOPA:\n"
+    "      SBIS %5, %6\n"  // Wait for sync high
+    "      RJMP LOOPA\n"
+    "      DEC %2\n"
+    "      BRNE LOOPA\n"
+    "LOOPB:\n"
+    "      SBIC %5, %6\n"  // Wait for sync low
+    "      RJMP LOOPB\n"
+    "      SBRS %0, 1\n"
+    "      RJMP SKIP2\n"
+    "      NOP\n"
+    "      NOP\n"
+    "      NOP\n"
+    "SKIP2:\n"
+    "      SBRC %0, 0\n"
+    "      RJMP SKIP1\n"
+    "SKIP1:\n"
+    "      NOP\n"
+    "      DEC %1\n"
+    "      BRNE SKIP1\n"
+    "      SBIC %5, %7\n"
+    "      SBI %3, %4\n"
+    "      CBI %3, %8\n"
+    "      NOP\n"
+    "      NOP\n"
+    "      NOP\n"
+    "      SBI %3, %8\n"
+    "      SEI\n"
+    : "+r" (lowBits), "+r" (fours), "+r" (counter)
+    : "I" (_SFR_IO_ADDR(PORTD)), "I" (PORTD4), "I" (_SFR_IO_ADDR(PIND)), "I" (PIND6), "I" (PIND5), "I" (PORTD3)
+  );
   delay(1); // Phosphor decay time
   digitalWrite(4, LOW);
-  return low;
 }
 
 short GetSyncTime()
@@ -171,7 +142,11 @@ void WaitForHSync()
 
 short CalculateDelay(short x)
 {
-  return (MICROSECONDS_TO_CYCLES(6) + MICROSECONDS_TO_CYCLES(x * 52 / 640)) / 3;
+  long cycles=x;
+  cycles=MICROSECONDS_TO_CYCLES(cycles*52);
+  cycles/=640;
+  cycles+=MICROSECONDS_TO_CYCLES(6);
+  return (short)cycles;
 }
 
 void loop()
