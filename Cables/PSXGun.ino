146c7f222bb1aa8313ce785198b46bf34f7a07f3
charcole <charlie@vyse.me.uk>
2018-08-04 18:57:46 +0100

    Working GunCon/Cinch PSX/PS2 gun

--- Cables/PSXGun.ino
+++ Cables/PSXGun.ino
@@ -9,13 +9,13 @@
 #define OUT_LED 2
 
 #define CONTROLLER_DATA_SIZE 8  // From LightGunVerter
-#define DATA_SIZE 4             // From PSX
+#define DATA_SIZE 8             // From PSX
 
 #include <SPI.h>
 
-uint8_t ReadMask[DATA_SIZE] = { 0xFF, 0xFF, 0, 0 };
-uint8_t ReadExpect[DATA_SIZE] = { 0x01, 0x42, 0, 0 };
-uint8_t Reply[DATA_SIZE] = { 0x41, 0x5A, 0xFF, 0xFF };
+uint8_t ReadMask[DATA_SIZE]   = { 0xFF, 0xFF, 0, 0, 0, 0, 0, 0 };
+uint8_t ReadExpect[DATA_SIZE] = { 0x01, 0x42, 0, 0, 0, 0, 0, 0 };
+uint8_t Reply[DATA_SIZE] = { 0x63, 0x5A, 0xFF, 0xFF, 0x01, 0x00, 0x05, 0x00 };
 
 uint8_t ControllerReadIndex = 0;
 uint8_t ControllerData[CONTROLLER_DATA_SIZE];
@@ -63,11 +63,6 @@ ISR (SPI_STC_vect)
     if (bDataGood)
     {
       SPDR = Reply[DataIndex];
-
-      if (DataIndex == (DATA_SIZE - 1))
-      {
-        WriteLEDHigh();
-      }
     
       WriteAckLow();
       DelayMicro();
@@ -90,32 +85,56 @@ ISR (SPI_STC_vect)
 
 void loop()
 {
-  if (DataIndex != 0 && ReadAttention() != 0)
+  if (ReadAttention() != 0)
   {
-    DataIndex = 0;
-    bDataGood = true;
-  }
-  if (Serial.available())
-  {
-    uint8_t SerialData = Serial.read();
-    if (SerialData == 0x80)
+    if (DataIndex != 0)
     {
-      if (ControllerReadIndex == CONTROLLER_DATA_SIZE) // Last lot of serial data was good
+      DataIndex = 0;
+      bDataGood = true;
+    }
+    if (Serial.available())
+    {
+      uint8_t SerialData = Serial.read();
+      if (SerialData == 0x80)
       {
-        if (ControllerData[0] == 0x80 && ControllerData[1] == 0x00) // Player 1 data
+        if (ControllerReadIndex == CONTROLLER_DATA_SIZE) // Last lot of serial data was good
         {
-          //digitalWrite(OUT_LED, (ControllerData[7]&0x1)?HIGH:LOW);
-          Reply[2] = ~ControllerData[6];
-          Reply[3] = ~ControllerData[7];
+          if (ControllerData[0] == 0x80 && ControllerData[1] == 0x00) // Player 1 data
+          {
+            uint16_t Buttons = 0xFFFF;
+            uint16_t GunX = (ControllerData[2] << 7) + ControllerData[3];
+            uint16_t GunY = (ControllerData[4] << 7) + ControllerData[5];
+            if (ControllerData[7] & (1<<0)) // Left
+              Buttons &= ~(1<<3);
+            if (ControllerData[7] & (1<<1)) // Right
+              Buttons &= ~(1<<14);
+            if (ControllerData[6] & (1<<3)) // B
+              Buttons &= ~(1<<13);
+            if (GunX == 0x3FFF && GunY == 0x3FFF)
+            {
+              GunX = 0x01;
+              GunY = 0x0A;
+            }
+            else
+            {
+              GunX = 0x4D + (((1023 - GunX) * 3)  >> 3);
+              GunY = 0x20 + ((GunY * 11) >> 5);
+            }
+            Reply[2] = Buttons & 0xFF;
+            Reply[3] = Buttons >> 8;
+            Reply[4] = GunX & 0xFF;
+            Reply[5] = GunX >> 8;
+            Reply[6] = GunY & 0xFF;
+            Reply[7] = GunY >> 8;
+          }
         }
+        ControllerReadIndex = 0;
       }
-      ControllerReadIndex = 0;
+      if (ControllerReadIndex < CONTROLLER_DATA_SIZE)
+      {
+        ControllerData[ControllerReadIndex] = SerialData;
+      }
+      ControllerReadIndex++;
     }
-    if (ControllerReadIndex < CONTROLLER_DATA_SIZE)
-    {
-      ControllerData[ControllerReadIndex] = SerialData;
-    }
-    ControllerReadIndex++;
   }
 }
-
